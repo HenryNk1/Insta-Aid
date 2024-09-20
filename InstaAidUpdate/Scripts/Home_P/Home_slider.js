be308deb229b7f722a99a4f748b180c210bba439
MezirmCoI <emeka232002@gmail.com>
2024-09-20 09:39:03 +0100

    Add files via upload

--- InstaAidUpdate/Scripts/Home_P/Home_slider.js
+++ InstaAidUpdate/Scripts/Home_P/Home_slider.js
@@ -12,37 +12,25 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
-// Function to redirect to 'soon.html' after showing the loader
 function redirectToSoon() {
     const loadingScreen = document.getElementById("loading-screen");
-    
-    // Show the loader
     loadingScreen.style.display = "flex"; 
-    
-    // Simulate a delay before redirecting (optional)
     setTimeout(function() {
-        window.location.href = "soon.html"; // Redirect to soon.html
-    }, 1000); // Adjust the time if needed
+        window.location.href = "soon.html";
+    }, 1000);
 }
 
-// Handle the chat form loading and redirect
+
 document.addEventListener("DOMContentLoaded", function () {
-    const chatForm = document.getElementById("chatForm");
+    const chatForm = document.querySelector(".chat-form");
     const loadingScreen = document.getElementById("loading-screen");
-
-    // Handle form submission for chat
     chatForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent default form submission
-
-        // Show the loading screen
+        event.preventDefault();
         loadingScreen.style.display = "flex";
-
-        // Simulate a delay for loading (e.g., 2 seconds)
         setTimeout(function () {
-            // After delay, redirect to Chatbox.html
             window.location.href = "../Html/Chatbox.html";
-        }, 2000); // 2-second delay
+        }, 2000);
     });
 });
+
 
